2cfdfe93e939fac7b5596a553985c28f28e99256
AhmedZohny208 <asfmz754@gmail.com>
2022-03-17 20:03:03 +0200

    fixed bug in payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,14 +49,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async session => {
-  try {
-    const tour = session.client_reference_id;
-    const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.amount_total / 100
-    await Booking.create({ tour, price, user })
-  } catch (err) {
-    console.log(err.message)
-  }
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100
+  await Booking.create({ tour, price, user })
 }
 
 exports.webhookCheckout = (req, res, next) => {
